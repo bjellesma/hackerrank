df4d64638633309360497057c270d6dafd9fb0a7
bjellesma <william.jellesma@gmail.com>
2016-09-13 17:03:48 -0400

    mistake: day 1

---
+++ 30 Days of Code/1 - Data Types/datatypes.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = 7
+d2 = 7.0
+s2 = 'is the best'
+
+i3 = int(raw_input(''))
+d3 = float(raw_input(''))
+s3 = raw_input('')
+
+print int(i + i3)
+print float(d + d3)
+print "%s%s" % (s, s3)
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
